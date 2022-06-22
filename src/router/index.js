b3799b692e298374ca2e0be960794088dbc1adb5
anlianaaguilar <anlianaaguilar@gmail.com>
2022-06-22 15:10:49 -0400

    se crea vista foundFighter

--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,40 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+// import HomeView from '../views/HomeView.vue'
 import Searcher from '@/views/Searcher.vue'
+import Fighters from '@/views/Fighters.vue'
+import FoundFighter from '@/views/FoundFighter.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'home',
+  //   component: HomeView
+  // },
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'peleadores',
+    component: Fighters,
   },
   {
     path: '/buscador',
+    name:'buscador',
     component: Searcher,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path:'/peleador_encontrado/:id', //ruta dinamica
+    component:FoundFighter,
   }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Fighters.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h1>Peleadores:</h1>
+        <ul v-for="fighter in fighters" :key="fighter">
+            <a @click="redirecTo(fighter.id)">
+                <li>{{fighter.nombre}}</li>
+            </a>
+        </ul>
+    </div>
+</template>
+
+<script>
+import data from '/peleadores.json'
+export default {
+    name: 'fighters-view',
+    // props: {},
+    data: function(){
+        return {
+            fighters:data
+        }
+    },
+    // computed: {},
+    methods: {
+
+
+        redirecTo(i){
+            this.$router.push(`/peleador_encontrado/${i}`)
+        }
+    },
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    
+</style>
