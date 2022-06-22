96d4c953d0d83291a90875ddfff562d3a97e2edc
anlianaaguilar <anlianaaguilar@gmail.com>
2022-06-22 13:52:33 -0400

    se crea vista searcher

---
+++ src/views/Searcher.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>Buscar un peleador por id</h1>
+        <input type="text" v-model="idFighter">
+        <button>Buscar</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'searcher-view',
+    // props: {},
+    data: function(){
+        return {
+            idFighter:"",
+        }
+    },
+    // computed: {},
+    //methods: {}
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    
+</style>
